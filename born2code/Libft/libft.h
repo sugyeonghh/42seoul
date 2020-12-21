24712fd0183a72948faadbf54481757662537608
sugyeonghh <sugyeong.py@gmail.com>
2020-12-21 19:18:13 +0900

    upload first subject

---
+++ born2code/Libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shong <shong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 16:32:35 by shong             #+#    #+#             */
+/*   Updated: 2020/12/21 18:21:32 by shong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+#include <string.h>
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+size_t	ft_strlen(const char *s);
+void	ft_putnbr_fd(int n, int fd);
+
+#endif
