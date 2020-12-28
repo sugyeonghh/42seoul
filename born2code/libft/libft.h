fc5cebac1804b23f18829c0bf70e15581fd2c06d
sugyeonghh <sugyeong.py@gmail.com>
2020-12-28 20:27:20 +0900

    upload

--- born2code/libft/libft.h
+++ born2code/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: shong <shong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 16:32:35 by shong             #+#    #+#             */
-/*   Updated: 2020/12/28 20:15:24 by shong            ###   ########.fr       */
+/*   Updated: 2020/12/28 20:27:05 by shong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
-void 				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap
