51e08f0913b35cbf0644d022095220dfad114b53
sugyeonghh <sugyeong.py@gmail.com>
2020-12-29 16:42:58 +0900

    update

--- born2code/libft/ft_strtrim.c
+++ born2code/libft/ft_strtrim.c
@@ -6,44 +6,22 @@
 /*   By: shong <shong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 15:52:53 by shong             #+#    #+#             */
-/*   Updated: 2020/12/29 12:57:32 by shong            ###   ########.fr       */
+/*   Updated: 2020/12/29 16:42:43 by shong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		ft_isset(char c, char const *set)
-{
-	while (*set)
-		if (*set++ == c)
-			return (1);
-	return (0);
-}
-
 char			*ft_strtrim(char const *s1, char const *set)
 {
-	char	*res;
-	int		i;
-	int		start;
-	int		end;
+	int		size;
 
 	if (!s1)
 		return (0);
-	i = -1;
-	start = 0;
-	end = ft_strlen(s1) - 1;
-	while (ft_isset(s1[++i], set))
-		;
-	start = i;
-	i = ft_strlen(s1);
-	while (ft_isset(s1[--i], set))
-		;
-	end = i;
-	if (start > end)
-		return (ft_strdup(""));
-	if (!(res = malloc(end - start + 2)))
-		return (0);
-	res = ft_memcpy(res, s1 + start, end - start + 1);
-	res[end - start + 1] = 0;
-	return (res);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	size = ft_strlen(s1);
+	while (size && ft_strchr(set, s1[size]))
+		size--;
+	return (ft_substr(s1, 0, size + 1));
 }
