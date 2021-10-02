136db6403d1220d656e95b382fa4f5ddbe8d8219
sugyeonghh <hsg980327@gmail.com>
2021-10-02 15:53:25 +0900

    upload pipex

---
+++ born2code/pipex/libft/ft_itoa.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shong <shong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/25 03:32:43 by shong             #+#    #+#             */
+/*   Updated: 2020/12/28 23:22:32 by shong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int		ft_size(long long ln)
+{
+	if (ln < 10)
+		return (1);
+	return (1 + ft_size(ln / 10));
+}
+
+char			*ft_itoa(int n)
+{
+	char		*res;
+	int			size;
+	int			sign;
+	long long	ln;
+
+	ln = n;
+	size = (ln >= 0) ? ft_size(ln) : ft_size(-ln) + 1;
+	if (!(res = malloc(size + 1)))
+		return (0);
+	sign = 0;
+	res[size--] = 0;
+	if (ln < 0)
+	{
+		sign = 1;
+		res[0] = '-';
+		ln *= -1;
+	}
+	while (size >= (sign ? 1 : 0))
+	{
+		res[size--] = ln % 10 + '0';
+		ln = ln / 10;
+	}
+	return (res);
+}
---
+++ born2code/pipex/libft/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shong <shong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 18:25:00 by shong             #+#    #+#             */
+/*   Updated: 2020/12/29 17:59:12 by shong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t		i;
+
+	if (!dst || !src)
+		return (0);
+	if (dstsize)
+	{
+		i = 0;
+		while (src[i] && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	return (ft_strlen(src));
+}
